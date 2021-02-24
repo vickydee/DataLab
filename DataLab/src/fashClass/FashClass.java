6ad1ffa8796f2f6e3b48b8bea0dde24ce63a3efc
vickydee <79496219+vickydee@users.noreply.github.com>
2021-02-24 10:18:56 -0700

    Update FashClass.java
    Began helper method addMethod, which takes in the scanned line from String[] clothes and adds 5 numbers from the array to a given ArrayList<String> (chosen by their label)

--- DataLab/src/fashClass/FashClass.java
+++ DataLab/src/fashClass/FashClass.java
@@ -9,11 +9,9 @@ import java.io.FileReader;
 import java.io.IOException; 
 
 public class FashClass {
-	private ArrayList<String> label;
-
 	private ArrayList<String> tee;
 	private ArrayList<String> trouser;
-	private ArrayList<String> pullover
+	private ArrayList<String> pullover;
 	private ArrayList<String> dress;
 	private ArrayList<String> coat;
 	private ArrayList<String> sandal;
@@ -23,6 +21,72 @@ public class FashClass {
 	private ArrayList<String> boot;
 	
 	FashClass() {
+		BufferedReader in 
+		 	= new BufferedReader(new FileReader("H:\\fashion-mnist_train.csv"));  
+		
+		 in.readLine();
+		  /* Similar to Scanner, nextLine(), this method will read the first line.
+		   * This will register the line in history and will skip the first line
+		   */
+
+		String nextLine = null; //declares String nextLine
+		String splitBy = ","; //delimiter 
+
+		//loop will run from 2nd line
+		 while ((nextLine = in.readLine()) != null)
+		 { 
+			 nextLine = in.readLine();
+			 String[] clothes += nextLine.split(splitBy);
+			  /* public String[] split(String x)
+			   * The method String.split() identifies the delimiter, splitting the string i.e. "johnny,sonny,fot" around matches of the given regular expression.
+			   * returns a String array.
+			   */     
+			
+			/* For the following: 
+			 * 0 T-shirt/top 
+			 * 1 Trouser 
+			 * 2 Pullover 
+			 * 3 Dress 
+			 * 4 Coat 
+			 * 5 Sandal 
+			 * 6 Shirt 
+			 * 7 Sneaker 
+			 * 8 Bag 
+			 * 9 Ankle boot
+			 */
+			if(clothes[0]==0)
+				addMethod(clothes, tee);
+			if(clothes[0]==1)
+				addMethod(clothes, trouser);
+			if(clothes[0]==2)
+				addMethod(clothes, pullover);
+			if(clothes[0]==3)
+				addMethod(clothes, dress);
+			if(clothes[0]==4)
+				addMethod(clothes, coat);
+			if(clothes[0]==5)
+				addMethod(clothes, sandal);
+			if(clothes[0]==6)
+				addMethod(clothes, shirt);
+			if(clothes[0]==7)
+				addMethod(clothes, sneaker);
+			if(clothes[0]==8)
+				addMethod(clothes, bag);
+			if(clothes[0]==9)
+				addMethod(clothes, boot);
+		}
+// method 1: find mean standard deviation of all data
+// method 2: find mean standard deviation between label
+	}
+	
+	private void addMethod(String[] arr, ArrayList<String> list) {
+		 for(int i = 0; i < arr.length; i += 157)
+		 {
+			 list.add((i));
+		 }		
+		for(int i = 0;) {
+			// arr[] = in.readLine();
+		// goal: if # then add to ArrayList
 		/* 0 T-shirt/top 
 		 * 1 Trouser 
 		 * 2 Pullover 
@@ -34,39 +98,8 @@ public class FashClass {
 		 * 8 Bag 
 		 * 9 Ankle boot
 		 */
-		
+		//
 
-		 BufferedReader in 
-		 	= new BufferedReader(new FileReader("H:\\fashion-mnist_train.csv"));  
-
-		//Similar to Scanner, nextLine(), this will read the first line
-		//This will register the line in history and will skip the first line
-		 in.readLine();
-
-		 String nextLine = null; //declare String nextLine
-		 String splitBy = ","; //delimiter 
-
-		//loop will run from 2nd line
-		 while (( nextLine = in.readLine() ) != null)
-		 { 
-		 nextLine = in.readLine();
-		 String[] clothes += nextLine.split(splitBy);
-		  /* public String[] split(String x)
-		   * The method String.split() identifies the delimiter, splitting the string i.e. "johnny,sonny,fot" around matches of the given regular expression.
-		   * returns a String array.
-		   */     
-		 for(int i = 0; i < clothes.length; i += 140)
-		 {
-		  if(i == 0) 
-		   label.add(clothes[i]);
-		 }
-		 }
-		
-		for(int i = 0;) {
-			// arr[] = in.readLine();
-		}
-// method 1: find mean standard deviation of all data
-// method 2: find mean standard deviation between label
 	}
 	
 	public static void main(String[] args) {
